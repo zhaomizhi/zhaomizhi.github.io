6c87c99ae144e56dff353ce13dfd960c0e4003a8
zhaomizhi <zhaomizhi@gmail.com>
2014-07-30 21:02:22 -0400

    header layout

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -21,6 +21,7 @@ $(document).ready(function() {
 
 var headheight = $("#entry-header").height();
 $("#entry-header").css("posistion","fixed");
+$("#entry-header").html('sss');
 $("#main").css("position","relative");
 $("#main").css("font-size","10px");
 
