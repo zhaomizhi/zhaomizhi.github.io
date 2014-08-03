8021a4ae8c7d51867f3812bd4adcb9abfb837e17
zhaomizhi <zhaomizhi@gmail.com>
2014-08-03 01:40:45 -0400

    back button

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
+	$("#backbutton").hide();
+
 	$(function(){
 		$(window).load(function() {
 		/* Act on the event */
 			resizeHeader();
 			$("#fittext1").fitText(2, { minFontSize: '10px', maxFontSize: '60px' });
-			});
+		});
 		$(window).resize(function(){
 			resizeHeader();
 			$("#fittext1").fitText(2, { minFontSize: '10px', maxFontSize: '80px' });
+		});
+		$(window).scroll(function(){
+
+			if( $(this).scrollTop() > 100 ) {
+				$("#backbutton").fadeIn("slow");
+			}else{
+				$("#backbutton").fadeOut("slow");
+			}
+		});
+
+		$("#backbutton").click(function(event){
+				
+				$('body').animate({scrollTop : 0 } , 600);
+				return false;
 			});
 	});
 
