457b80bc93e99e8d46dd99e37f55efee5d9585d7
zhaomizhi <zhaomizhi@gmail.com>
2014-08-03 01:02:15 -0400

    test-index layout

---
+++ assets/js/test.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$(function(){
+		$(window).load(function() {
+		/* Act on the event */
+			resizeHeader();
+			$("#fittext1").fitText(2, { minFontSize: '10px', maxFontSize: '60px' });
+			});
+		$(window).resize(function(){
+			resizeHeader();
+			$("#fittext1").fitText(2, { minFontSize: '10px', maxFontSize: '80px' });
+			});
+	});
+
+});
+
+function resizeHeader(){
+	var heightImage = $(".unmoved-header").height();
+	if(heightImage <= 500){
+		$("#homecontent").css('top', heightImage -10 );
+	}else{
+		$("#homecontent").css('top', 490);
+	}
+	$("#fittext1").css('top', '35%');
+	var widthContent = $("#homecontent").width() + 20;
+	$("#homecontent").width(widthContent);
+}
