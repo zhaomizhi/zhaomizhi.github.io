e392d221faa7d02796628f68fbb73e0a15464aad
zhaomizhi <zhaomizhi@gmail.com>
2014-08-05 17:44:11 -0400

    footresponsive function

--- assets/js/test.js
+++ assets/js/test.js
@@ -71,6 +71,7 @@ function footerPosition(){
 }
 
 function fontResponsive(){
+	var fit = $("#fittext1");
 	if($("#fittext1") !== undefined){
 	$("#fittext1").fitText(2, { minFontSize: '35px', maxFontSize: '90px' });
 	$(".entry-title").fitText(2, { minFontSize: '23px', maxFontSize: '32px' });
