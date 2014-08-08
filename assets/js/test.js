f3806897883e5dbce803c86a7975e72c976a4640
zhaomizhi <zhaomizhi@gmail.com>
2014-08-08 17:10:46 -0400

    test font size

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,4 +1,36 @@
 
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
 
 
 $(document).ready(function() {
@@ -9,7 +41,7 @@ $(document).ready(function() {
 		/* Act on the event */
 			resizeHeader();
 			resizePageContent();
-
+			fontResponsive();
 			showHeight();
 			
 			
@@ -82,7 +114,6 @@ function fontResponsive(){
 		$(".entry-title")[1].fitText(2, { minFontSize: '23px', maxFontSize: '32px' });
 		return;
 	}
-	
 	
 }
 function showHeight(){
