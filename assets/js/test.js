6ab59513a2abbc5b51e89baff483b606af5c1e1e
zhaomizhi <zhaomizhi@gmail.com>
2014-08-08 16:58:56 -0400

    test font size

--- assets/js/test.js
+++ assets/js/test.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 			resizePageContent();
 
 			showHeight();
-			fontResponsive();
+			
 			
 		});
 
@@ -19,7 +19,7 @@ $(document).ready(function() {
 			resizeHeader();
 			resizePageContent();
 			showHeight();
-			fontResponsive();
+		
 	
 			
 		});
